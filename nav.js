1f6257ff29fa77d2cacefaf5823aa8c5c9209583
aniketparate <89774693+aniketparate@users.noreply.github.com>
2022-07-26 19:58:21 +0530

    feat: fixed navbar dropdown issue

--- nav.js
+++ nav.js
@@ -48,30 +48,25 @@
         });
     })(bootstrap);
 
-    // var expandCollapse = function(){
-    //     if ( $(window).width() > 1200 ) {
-    //         $(function(){
-    //             $('#drop-down').removeAttr('data-bs-toggle');
-    //             // // add a class .collapse to a div .showHide
-    //             // $('.showHide').addClass('collapse');
-    //             // // set display: "" in css for the toggle button .btn.btn-primary
-    //             // $('button.btn.btn-primary').css('display', '');// removes display property to make it visible
-    //         });
-    //     }
-    //     // else {
-    //     //     $(function(){
-    //     //         // remove a class .collapse from a div .showHide
-    //     //         $('.showHide').removeClass('collapse');
-    //     //         // set display: none in css for the toggle button .btn.btn-primary  
-    //     //         $('button.btn.btn-primary').css('display', 'none');// hides button display on bigger screen
-    //     //     });
-    //     // }
-    // }
-    // $(window).resize(expandCollapse); // calls the function when the window first loads
-
-$(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth > 1200) {
-        $("#drop-down").removeAttr("data-bs-toggle");
+    var expandCollapse = function(){
+        if ( $(window).width() > 1200 ) {
+            $(function(){
+                $('.drop-down').removeAttr('data-bs-toggle');
+                // $('.drop-down').removeAttr('data-bs-popper');
+                console.log(1)
+                // // add a class .collapse to a div .showHide
+                // $('.showHide').addClass('collapse');
+                // // set display: "" in css for the toggle button .btn.btn-primary
+                // $('button.btn.btn-primary').css('display', '');// removes display property to make it visible
+            });
+        }
+        // else {
+        //     $(function(){
+        //         // remove a class .collapse from a div .showHide
+        //         $('.showHide').removeClass('collapse');
+        //         // set display: none in css for the toggle button .btn.btn-primary  
+        //         $('button.btn.btn-primary').css('display', 'none');// hides button display on bigger screen
+        //     });
+        // }
     }
-});
+    $(window).resize(expandCollapse); // calls the function when the window first loads
